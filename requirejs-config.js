2815fb30dbf27629eb06492e3c9de5aced6850f2
Dangduc2007 <nguyendangduc2007@gmail.com>
2021-09-09 18:39:24 +0700

    abc

---
+++ requirejs-config.js
@@ -0,0 +1,15 @@
+var config = {
+    paths: {
+        slick:        'js/slick',
+        owlCarousel:        'js/owl.carousel'
+    },
+    shim: {
+        slick: {
+            deps: ['jquery']
+        },
+        owlCarousel: {
+            deps: ['jquery']
+        }
+    }
+    
+};
---
+++ web/js/custom.js
@@ -0,0 +1,81 @@
+
+require([
+    'jquery',
+    'slick',
+    'accordion',
+    'owlCarousel'
+], function ($) {
+    $(document).ready(function () {
+
+        $('.footer-showroom-content').slick({
+            dots: false,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+
+            responsive: [{
+                breakpoint: 768,
+                settings: {
+                    dots: false,
+                    arrows: false,
+                    infinite: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }]
+        });
+
+        $('.banner-bottom').slick({
+            dots: false,
+            slidesToShow: 2,
+            slidesToScroll: 1,
+
+            responsive: [{
+                breakpoint: 640,
+                settings: {
+                    dots: false,
+                    arrows: false,
+                    infinite: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }]
+        });
+
+        $('.slider-wrap').slick({
+            dots: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            responsive: [{
+                breakpoint: 500,
+                settings: {
+                    dots: false,
+                    arrows: false,
+                    infinite: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }]
+        });
+        
+        // Accordion---------------
+
+        if ($(window).width() < 768) {
+            $(".footer-middle-content").accordion({
+               "openedState": "active",
+               "collapsible": true,
+               "active": [2],
+               "multipleCollapsible": true,
+               "animate":{
+      
+                  timing:"ease-out",
+                  duration:"500",
+                  delay:"1s"
+               }
+            });
+        }
+
+    });
+    
+    
+});
